042a92326ff501474f655f159cebb7ecd492e65e
Lucky Agarwal <67011698@kmitl.ac.th>
2025-06-24 09:46:44 +0000

    fixed

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 n = input("Give me a number: ")
 try:
-    if int(n):
+    x = float(n)
+    if x == int(x):
         print("The number is an integer")
-except:
-    if float(n):
+    else:
         print("The number is a decimal")
+except ValueError:
+        print("Please enter a valid number")
