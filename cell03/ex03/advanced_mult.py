fbe3b9e8cf6661ec06231db218eccb9c8d69713f
Lucky Agarwal <67011698@kmitl.ac.th>
2025-06-24 08:38:32 +0000

    edit

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-for i in range(11):
-    print(f"\nTable de {i}:", end = '')
-    for j in range(11):
-        print(f" {i * j}", end='')
+import sys
+x = len(sys.argv)
+if x > 1:
+    print("none")
+else:
+    for i in range(11):
+        print(f"\nTable de {i}:", end = '')
+        for j in range(11):
+            print(f" {i * j}", end='')
