1792b05fa2c8680ab6052a29f147bdc5e9e0c6df
Lucky Agarwal <67011698@kmitl.ac.th>
2025-06-25 12:44:27 +0000

    rush

---
+++ rush/ex00/main.py
@@ -0,0 +1,17 @@
+from checkmate import checkmate
+
+def main():
+    board = """\
+.Q...B
+......
+..K.P.
+......
+R.P...
+...R.B\
+"""
+
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
+    
